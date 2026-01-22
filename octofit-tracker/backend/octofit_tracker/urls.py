17a53581180a087dd050ae55e894a8670333425d
xabierel13 <xabier.e.l.13@gmail.com>
2026-01-22 10:11:05 +0000

    fuuul

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.api_root, name='api-root'),
+    path('api/', views.api_root, name='api-root'),
+    # path('api/', include(router.urls)),  # Uncomment and configure if using DRF routers
 ]
